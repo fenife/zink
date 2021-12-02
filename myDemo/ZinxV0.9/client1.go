bf27c6dd2eed5f2c4c8584505f1ed4b8b55a2c7e
fenife <fenife@163.com>
2021-12-02 21:41:56 +0800

    42-ZinxV0.9-链接管理模块的功能测试

---
+++ myDemo/ZinxV0.9/client1.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"time"
+	"zink/znet"
+)
+
+/*
+ 模拟客户端
+*/
+func main() {
+	fmt.Println("client1 start ...")
+	time.Sleep(1 * time.Second)
+	// 1.直接连接远程服务器，得到一个conn连接
+	conn, err := net.Dial("tcp", "127.0.0.1:8999")
+	if err != nil {
+		fmt.Println("client start err, exit!")
+		return
+	}
+
+	//2 连接调用Write 写数据
+	for {
+		//发送封包的message消息
+		dp := znet.NewDataPack()
+		binaryMsg, err := dp.Pack(znet.NewMsgPackage(1, []byte("zinx client1 Test Message")))
+		if err != nil {
+			fmt.Println("pack error", err)
+			return
+		}
+
+		_, err = conn.Write(binaryMsg)
+		if err != nil {
+			fmt.Println("write error", err)
+			return
+		}
+
+		//服务器应该给我们回复一个message数据， MsgID: 0, pingpingping
+		//先读取流中的head部分 得到ID 和 dataLen
+		binaryHead := make([]byte, dp.GetHeadLen())
+		if _, err := io.ReadFull(conn, binaryHead); err != nil {
+			fmt.Println("read head error", err)
+			break
+		}
+
+		//将二进制的head拆包到msg结构体中
+		msgHead, err := dp.Unpack(binaryHead)
+		if err != nil {
+			fmt.Println("client unpack msgHead error", err)
+			break
+		}
+		if msgHead.GetMsgLen() > 0 {
+			//再根据DataLen进行第二次读取，将data读出来
+			msg := msgHead.(*znet.Message)
+			msg.Data = make([]byte, msg.GetMsgLen())
+			if _, err := io.ReadFull(conn, msg.Data); err != nil {
+				fmt.Println("read msg data error", err)
+				return
+			}
+			fmt.Printf("---> recv server msg: id = %d, len = %d, data = %s\n",
+				msg.Id, msg.DataLen, msg.Data)
+
+		}
+
+		// cpu 阻塞
+		time.Sleep(3 * time.Second)
+	}
+
+}
---
+++ myDemo/ZinxV0.9/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"zink/ziface"
+	"zink/znet"
+)
+
+/*
+ 基于Zinx框架来开发的 服务器端应用程序
+*/
+
+//ping test 自定义路由
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+type HelloZinxRouter struct {
+	znet.BaseRouter
+}
+
+
+// Test Handle
+func (br *PingRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call PingRouter Handle...")
+	//读取客户端的数据，再回写ping
+	fmt.Printf("recv from client: msgId = %d, data = %s\n",
+		request.GetMsgID(), request.GetData())
+	err := request.GetConnection().SendMsg(request.GetMsgID(), []byte("ping router"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (br *HelloZinxRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call HelloZinxRouter Handle...")
+	//读取客户端的数据，再回写ping
+	fmt.Printf("recv from client: msgId = %d, data = %s\n",
+		request.GetMsgID(), request.GetData())
+	err := request.GetConnection().SendMsg(request.GetMsgID(), []byte("hello zinx router"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+
+func main() {
+	// 1.创建一个server句柄，使用Zinx的api
+	s := znet.NewServer("[zink v0.9]")
+	//2.给当前zinx添加自定义的router
+	s.AddRouter(0, &PingRouter{})
+	s.AddRouter(1, &HelloZinxRouter{})
+	// 3.启动server
+	s.Serve()
+}
